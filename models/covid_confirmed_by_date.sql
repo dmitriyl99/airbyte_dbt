7e67cfa5fbfd8eb5e4abdbdecbde2783d3d1b402
Dmitriy <dmitriyl1899@gmail.com>
2022-11-14 15:04:12 +0500

    fix database name

--- models/covid_confirmed_by_date.sql
+++ models/covid_confirmed_by_date.sql
@@ -4,7 +4,7 @@ with covid_confirmed_by_date as (
     SELECT 
         date as date,
         SUM(new_confirmed) as sum_of_confirmed
-    FROM covid.covid_epidemilolgy
+    FROM covid_epidemilolgy
     GROUP BY date
 )
 
