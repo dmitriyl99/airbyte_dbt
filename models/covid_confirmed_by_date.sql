7a4d542d9a99b16dcab1690c391f42104f7390a7
Dmitriy <dmitriyl1899@gmail.com>
2022-11-14 15:00:11 +0500

    add covid confirmed by date model

---
+++ models/covid_confirmed_by_date.sql
@@ -0,0 +1,11 @@
+{{ config(materialized='table') }}
+
+with covid_confirmed_by_date as (
+    SELECT 
+        date as date,
+        SUM(new_confirmed) as sum_of_confirmed
+    FROM covid.covid_epidemilolgy
+    GROUP BY date
+)
+
+SELECT * FROM covid_confirmed_by_date;
